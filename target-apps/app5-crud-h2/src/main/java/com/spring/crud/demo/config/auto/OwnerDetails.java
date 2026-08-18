0d73e112b4057516c54e6c36baf53d29500cb3a0
Elyas Amiri <3154658a@student.gla.ac.uk>
2026-08-18 13:46:43 +0100

    Refactor app5-crud-h2

--- target-apps/app5-crud-h2/src/main/java/com/spring/crud/demo/config/auto/OwnerDetails.java
+++
@@ -1,9 +0,0 @@
-package com.spring.crud.demo.config.auto;
-
-import lombok.Data;
-
-@Data
-public class OwnerDetails {
-    private String name;
-    private String email;
-}
--- target-apps/app5-crud-h2/src/main/java/com/spring/crud/demo/repository/SportsIconRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.crud.demo.repository;
-
-import com.spring.crud.demo.model.SportsIcon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SportsIconRepository extends JpaRepository<SportsIcon, Integer> {
-
-}
